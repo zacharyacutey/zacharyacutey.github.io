3e71cb941a53694b54da39bef9c273a4ac3a0f20
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-03-26 16:25:17 -0400

    Update utilities.js

--- utilities.js
+++ utilities.js
@@ -1,1 +1,13 @@
-function 
+function GetPair(pair,item)
+{
+  var i = 0;
+  while(i!=pair.length)
+  {
+    if(pair[i][0]==item)
+    {
+      return pair[i][1];
+    }
+    i++;
+  }
+  throw new Error("Match not found!");
+}
