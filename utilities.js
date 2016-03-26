e501a5dae48f814d8de68589ccc6a86cdbb63498
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-03-26 17:28:24 -0400

    Update utilities.js

--- utilities.js
+++ utilities.js
@@ -7,8 +7,7 @@ function Folder()
   this.values=function(){
     return this.contents.map(function(i){return i[1];});
   };
-  this.access=function(key)
-  {
+  this.access=function(key){
     var i = 0;
     while(i != this.contents.length)
     {
@@ -19,5 +18,17 @@ function Folder()
       i++;
     }
     throw Error("Key not in Object!");
+  };
+  this.define=function(key,value){
+    var i = 0;
+    while(i != this.contents.length)
+    {
+      if(this.keys()[i]==key)
+      {
+        this.contents[i][1]=value;
+        return;
+      }
+    }
+    this.contents[this.contents.length]=[key,value];
   };
 }
