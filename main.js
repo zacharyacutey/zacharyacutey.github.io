36021a10cc29e3ff5101f4c3aebfe8f62d930d30
Zacharý <zacharyacutey@users.noreply.github.com>
2016-08-16 17:19:18 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 var SIZE = 3; //The size of the array of boxes
 var JUMP_HEIGHT = 2; //The height that the player can jump.
+
+
 var x = 0; //The x position
 var y = 0; //The y position
+
+var direction=0; //1 is up, 0 is no vertical movement, -1 is downards vertical
+var increments=0; //The number of movements to go till it stop
+
 function NOTHING_TEXT(x,y){return "<div class='nothing' id='p"+x+"_"+y+"'></div>"; } //The div with nothing in it
 function PLAYER_TEXT(x,y){return "<div class='player' id='p"+x+"_"+y+"'></div>"; } //The divs with the player in it
 function OBSTACLE_TEXT(x,y){return "<div class='obstacle' id='p"+x+"_"+y+"'></div>"; } //The divs with the platforms
 function init_display() //returns the initial text
 {
-	var i = SIZE-1;
+	var i = SIZE-1; //This is modified old code, don't want to comment it all out
 	var j = 0;
 	var res = "";
 	while(i!=-1)
@@ -23,10 +29,22 @@ function init_display() //returns the initial text
 	}
 	return res;
 }
-function display()
+function display() //Displays the 
 {
 	document.getElementsByClassName("player")[0].className="nothing";
 	document.getElementById("p"+x+"_"+y).className="player";
+}
+function leftArrow()
+{
+}
+function rightArrow()
+{
+}
+function upArrow()
+{
+}
+function otherKey()
+{
 }
 function keydown(event)
 {
