2f0442de35905f1d0790ef1c152acac9613bc318
Zachary T <zacharyacutey@users.noreply.github.com>
2017-10-14 18:27:23 -0400

    Update paddle.js

--- Breakout/paddle.js
+++ Breakout/paddle.js
@@ -2,6 +2,8 @@ function drawPaddle() {
     ctx.beginPath();
     ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
     ctx.fillStyle = color;
+    ctx.strokeStyle = "#FF0000";
     ctx.fill();
+    ctx.stroke();
     ctx.closePath();
 }
