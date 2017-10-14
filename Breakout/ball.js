d6d480a0df079c99efa85b6ee87c87a76308ebd7
Zachary T <zacharyacutey@users.noreply.github.com>
2017-10-14 18:31:29 -0400

    Update ball.js

--- Breakout/ball.js
+++ Breakout/ball.js
@@ -3,6 +3,8 @@ function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = color;
+    ctx.strokeStyle = "#FF0000";
+    ctx.stroke();
     ctx.fill();
     ctx.closePath();
 }
