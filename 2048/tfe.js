f818d9559ab9405d19e5d54fa5a01ea0012debd9
Zachary T <zacharyacutey@users.noreply.github.com>
2018-01-06 16:50:53 -0500

    Create tfe.js

---
+++ 2048/tfe.js
@@ -0,0 +1,117 @@
+//CONSTANTS
+
+var SIZE = 4;
+
+//HELPER FUNCTIONS
+
+function merge(list) {
+	var temp = [];
+	for(var i = 0;i < list.length;i++) //Remove all zeros
+	{
+		if(list[i] != 0)
+			temp = temp.concat(list[i]);
+	}
+	while(temp.length != list.length) { //Pad with zeros
+		temp = temp.concat(0);
+	}
+	for(var i = 1; i < list.length; i++) { //Merge
+		if(temp[i-1] == temp[i]) {
+			temp[i]=0;
+			temp[i-1]*=2;
+		}
+	}
+	var temp2 = []; //We need to create a new list to do the first two steps again
+	for(var i = 0;i < list.length;i++) //Remove all zeros
+	{
+		if(temp[i] != 0)
+			temp2 = temp2.concat(temp[i]);
+	}
+	while(temp2.length != list.length) { //Pad with zeros one last time
+		temp2 = temp2.concat(0);
+	}
+	return temp2;
+}
+
+function mergeLeft(array) {
+	return array.map(merge);
+}
+
+function mergeRight(array) {
+	return array.map(function(x){
+		x.reverse();
+		var res = merge(x).reverse();
+		x.reverse();
+		return res;
+	});
+}
+
+function transpose(array) {
+	var res = Array(array[0].length).fill(0).map(function(i){return [];});
+	for(var i = 0; i < array.length; i++) {
+		for(var j = 0;j < array[0].length;j++) {
+			res[j][i] = array[i][j];
+		}
+	}
+	return res;
+}
+
+function mergeUp(array) {
+	return transpose(mergeLeft(transpose(array)));
+}
+
+function mergeDown(array) {
+	return transpose(mergeRight(transpose(array)));
+}
+
+function randomElement(list) {
+	//this is a temporary fix.
+	console.log("LIST: "+list);
+	console.log("LENGTH: "+list.length);
+	var index = list.length + 1;
+	while(list[index] === undefined) {
+		index = Math.floor(Math.random()*(list.length + 1));
+		console.log("INDEX: "+index);
+	}
+	return list[index];
+}
+
+function insertNumber(array) { //Modifies the array
+	var zeros = [];
+	for(var i = 0; i < array.length; i++) {
+		for(var j = 0; j < array[0].length; j++) {
+			if(array[i][j] == 0) {
+				zeros = zeros.concat([[i,j]]);
+			}
+		}
+	}
+	var index = randomElement(zeros);
+	var number = randomElement([2,2,2,2,2,2,2,2,2,4]);
+	array[index[0]][index[1]] = number;
+}
+
+function gameOver(array) {
+	return mergeLeft(array) == array && mergeRight(array) == array && array == mergeUp(array) && mergeDown(array) == array;
+}
+
+function generateArray() {
+	var arr = Array(SIZE).fill(0).map(function(n){return Array(SIZE).fill(0);});
+	insertNumber(arr);
+	insertNumber(arr);
+	return arr;
+}
+
+
+// ------------- BEGIN (TEMPORARY) UI STUFFS ---------------
+function temporaryGame() {
+	var board = generateArray();
+	while(!gameOver(board)) {
+		var choice = prompt(board.map(function(n){return n.join('\t');}).join('\n'));
+		if((choice == 'a' || choice == 'A') && mergeLeft(board)!=board) board = mergeLeft(board);
+		else if((choice == 'w' || choice == 'W') && mergeUp(board)!=board) board = mergeUp(board);
+		else if((choice == 's' || choice == 'S') && mergeDown(board)!=board) board = mergeDown(board);
+		else if((choice == 'd' || choice == 'd') && mergeRight(board)!=board) board = mergeRight(board);
+		else if(choice == 'q' || choice == 'Q') break;
+		else continue;
+		insertNumber(board);
+	}
+}
